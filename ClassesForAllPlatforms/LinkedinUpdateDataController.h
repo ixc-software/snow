7ec8118504d09186ff55c47005ef08a8ceac1c6d
Oleksii Vinogradov <oleksiivinogradov@mac.com>
2012-03-06 13:18:42 +0200

    linkedin

---
+++ ClassesForAllPlatforms/LinkedinUpdateDataController.h
@@ -0,0 +1,13 @@
+//
+//  LinkedinUpdateDataController.h
+//  snow
+//
+//  Created by Oleksii Vynogradov on 3/5/12.
+//  Copyright (c) 2012 IXC-USA Corp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LinkedinUpdateDataController : NSObject
+
+@end
---
+++ ClassesForAllPlatforms/LinkedinUpdateDataController.m
@@ -0,0 +1,13 @@
+//
+//  LinkedinUpdateDataController.m
+//  snow
+//
+//  Created by Oleksii Vynogradov on 3/5/12.
+//  Copyright (c) 2012 IXC-USA Corp. All rights reserved.
+//
+
+#import "LinkedinUpdateDataController.h"
+
+@implementation LinkedinUpdateDataController
+
+@end
